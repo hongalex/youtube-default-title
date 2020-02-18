c08dc21b514f0af78b7ede2f7279d5e19790d1d7
Alex Hong <hongalex@google.com>
2020-02-18 01:15:28 -0800

    feat: replace titles for new page loads using message passing

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
-chrome.webNavigation.onCompleted.addListener(function(data) {
-  if(data)
-    chrome.tabs.sendMessage(data.tabId, {}, function(response) {
-      console.log(response)
-    });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  // only listen for Youtube video pages. 
+  if (!tab.url.includes("https://www.youtube.com/watch")){
+    return
+  }
+
+  if (tab.active) {
+    let title = tab.title
+    if (title) {
+      chrome.tabs.sendMessage(tab.id, {"title": title}, function(response) {
+        console.log(response)
+      });
+    }
+  }
 });
--- content.js
+++ content.js
@@ -1,10 +1,14 @@
-let docTitle = document.title
-let untranslatedTitle = docTitle.split(" - YouTube")[0]
-
-let header = document.getElementsByClassName("title")[0]
-let titleElement = header.getElementsByClassName("ytd-video-primary-info-renderer")[0]
-titleElement.innerHTML = untranslatedTitle
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  sendResponse(document.title);
+  let untranslatedTitle = request.title.split(" - YouTube")[0];
+  let header = document.getElementsByClassName("title")[0];
+  if (header) {
+    let titleElement = header.getElementsByClassName(
+      "ytd-video-primary-info-renderer"
+    )[0];
+    let translatedTitle = titleElement.textContent;
+    titleElement.innerHTML = untranslatedTitle;
+    console.log("Original", untranslatedTitle);
+    console.log("Translated: ", translatedTitle);
+  }
+  sendResponse("Received message");
 });
